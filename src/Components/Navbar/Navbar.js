5e23794588cc81d92ddc8a6c866e30119c06e022
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-10 09:11:22 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+const Navbar = () => {
+    return (
+        <div>
+        <nav  className="md:flex bg-gray-800   sticky   gap-2 justify-center md:justify-between  py-5 px-5">
+   <div className='text-4xl font-bold text-white'>
+     <h1>Gadget Store</h1>
+   </div>
+   <div className='text-xl text-white md:flex gap-3'>
+     <CustomLink
+       to='/'          >
+       Home
+     </CustomLink>
+     <CustomLink
+                 to='/blog'
+     >
+       Blog
+     </CustomLink>
+     <CustomLink
+      
+      to='/checkout'
+    >
+      Checkout
+    </CustomLink>
+
+     <CustomLink
+      
+       to='/about'
+     >
+       About 
+     </CustomLink>
+     <CustomLink
+      
+      to='/signup'
+    >
+      Sign Up
+    </CustomLink>
+
+    <CustomLink
+      
+      to='/login'
+    >
+      Login
+    </CustomLink>
+       
+   </div>
+ </nav>
+   </div>
+    );
+};
+
+export default Navbar;
