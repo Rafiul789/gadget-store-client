99c5b46b90d6690a60c18a106468e7b3733a20a9
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-10 21:49:51 +0600

    extra section feedback added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
+import Feedback from '../Feedback/Feedback';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
     return (
@@ -9,8 +11,8 @@ const Home = () => {
 
 <div className=" my-5 text-center "> <h1 className="text-4xl">Welcome to    <span className="text-4xl text-blue-600"> Gadget Store  </span></h1>  </div>
 
-
-
+<Feedback></Feedback>
+<Footer></Footer>
         </div>
     );
 };
