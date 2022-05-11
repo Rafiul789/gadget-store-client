53422d61a9f4980393f275e00aca81e57aab10d4
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-11 22:30:35 +0600

    requireauth added

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ import Navbar from './Components/Navbar/Navbar';
 import NotFound from './Components/NotFound/NotFound';
 import Signup from './Components/Signup/Signup';
 import Contact from './Components/Contact/Contact';
+import Inventory from './Components/Inventory/Inventory';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
 
 function App() {
   return (
     <div >
     <Navbar></Navbar>
     <Toaster></Toaster>
-    <Routes> <Route path='/' element={<Home></Home>}> </Route> <Route path='/blogs' element={<Blog></Blog>} ></Route>  <Route path='/login' element={<Login></Login>} ></Route> <Route path='/signup' element={<Signup></Signup>}></Route> <Route path='/contact' element={<Contact></Contact>}></Route>    <Route path='*' element={<NotFound></NotFound>} ></Route>    </Routes>
+    <Routes> <Route path='/' element={<Home></Home>}> </Route> <Route path='/blogs' element={<Blog></Blog>} ></Route>
+    <Route path='/inventory' element={<RequireAuth>  <Inventory></Inventory> </RequireAuth>  }></Route>
+      <Route path='/login' element={<Login></Login>} ></Route> <Route path='/signup' element={<Signup></Signup>}></Route> <Route path='/contact' element={<Contact></Contact>}></Route>    <Route path='*' element={<NotFound></NotFound>} ></Route>    </Routes>
 
     </div>
   );
