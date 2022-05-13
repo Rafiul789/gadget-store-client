79e391cf5808c0d6fb92c536eec63d56432cb0ff
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-13 14:43:03 +0600

    add item functionality added with mongodb database

--- src/Components/AddItem/addItem.js
+++ src/Components/AddItem/addItem.js
@@ -3,15 +3,31 @@ import { useForm } from "react-hook-form";
 const Additem = () => {
 
     const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = data =>{
+    console.log(data);
+const url=`http://localhost:5000/service`;
+fetch(url,{
+    method: 'POST',
+    headers:{
+        'content-type': 'application/json'
+    },
+    body:JSON.stringify(data)
+})
+.then(res => res.json())
+.then(result => {console.log(result);})
+
+
+} ;
     return (
-        <div className="  mx-auto  items-center justify-center">
+        <div className="  mx-16  items-center justify-center">
             <h1 className="text-4xl my-5">Please add your item</h1>
-            <form className="d-flex flex-column"  onSubmit={handleSubmit(onSubmit)}>
-      <input className="border-2 border-black gap-2 " {...register("firstName", { required: true, maxLength: 20 })} />
-      <input className="border-2 border-black  gap-2 "  {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-      <input className="border-2 border-black  gap-2 "   type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" />
+            <form className="grid grid-cols-3 gap-4"  onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder="product name" className="border-2 border-black gap-2 " {...register("name", { required: true, maxLength: 20 })} />
+      <input placeholder="description"  className="border-2 border-black  gap-2 "  {...register("description", { pattern: /^[A-Za-z]+$/i })} />
+      <input placeholder="price" className="border-2 border-black  gap-2 "   type="number" {...register("price")} />
+      <input placeholder="img url" className="border-2 border-black  gap-2 "   type="text" {...register("img")} />
+
+      <input className="border-1 bg-blue-500 text-white"  type="submit" value="add item" /> 
     </form>
 
         </div>
