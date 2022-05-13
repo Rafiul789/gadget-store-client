8841b0662bbbb176fea36ae65d8983d5567d72cc
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-13 14:11:54 +0600

    react form added

--- src/Components/AddItem/addItem.js
+++ src/Components/AddItem/addItem.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+const Additem = () => {
 
-const addItem = () => {
+    const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
     return (
-        <div>
-            <h1>Please add your item</h1>
+        <div className="  mx-auto  items-center justify-center">
+            <h1 className="text-4xl my-5">Please add your item</h1>
+            <form className="d-flex flex-column"  onSubmit={handleSubmit(onSubmit)}>
+      <input className="border-2 border-black gap-2 " {...register("firstName", { required: true, maxLength: 20 })} />
+      <input className="border-2 border-black  gap-2 "  {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input className="border-2 border-black  gap-2 "   type="number" {...register("age", { min: 18, max: 99 })} />
+      <input type="submit" />
+    </form>
+
         </div>
     );
 };
 
-export default addItem;
+export default Additem;
